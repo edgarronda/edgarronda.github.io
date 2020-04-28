5716464479163ec89380553731ca6ca37b86ded2
Edgar Ronda <info@edgarantonio.com>
2020-04-28 14:19:31 -0700

    Removing jQuery, automatically show up Modal with information.

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,23 @@
-onload{
-	
-}
+document.addEventListener("DOMContentLoaded", function () {
+	var elems = document.querySelectorAll(".modal");
+	var instances = M.Modal.init(elems, {});
+	// display a warning in the console
+	console.log(
+		"%cHOLA DEV!",
+		"color: red;font-size:2.5rem;font-weight:bold;"
+	);
+	console.log(
+		"%cPodemos colaborar, enviame un mensaje.",
+		"font-size:1.25rem;line-height:1.1;margin-top:.5em"
+	);
+	console.log(
+		"%c--Ed Ronda.--",
+		"font-size:1.25rem;line-height:1.1;margin-top:.5em"
+	);
+
+	setTimeout(() => {
+		console.log("World!");
+		var link = document.getElementById("modalstats");
+		link.click();
+	}, 3500);
+});
